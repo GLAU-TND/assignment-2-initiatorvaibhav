b630b7b3fc614767941e6ba48a4bd8f306a4898e
Vaibhav <vaibhav.singh_cs18@gla.ac.in>
2020-03-31 16:44:14 +0530

    generating setter of left, right and Data

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -11,6 +11,18 @@ import java.util.Scanner;
 public class TreeNode {
     private TreeNode left;
 
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
     public TreeNode getLeft() {
         return left;
     }
