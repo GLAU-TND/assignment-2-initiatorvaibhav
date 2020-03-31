04b05d6a3a368d91e722da1615c7ac0ad2acbd0d
Vaibhav <vaibhav.singh_cs18@gla.ac.in>
2020-03-31 18:55:02 +0530

    declaring getterv and setter in mybst

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -14,15 +14,63 @@ public class MyBinarySearchTree {
     private TreeNode newNode;
     private TreeNode root;
     private TreeNode temp;
-    private int count;
+    private static int count;
     private MyQueue prefix;
     private MyQueue postfix;
+
+    public static int getCount() {
+        return count;
+    }
+
+    public static void setCount(int count) {
+        MyBinarySearchTree.count = count;
+    }
 
     public MyBinarySearchTree() {
         this.root = root;
         this.temp = temp;
         this.count = 0;
         this.prefix = prefix;
+        this.postfix = postfix;
+    }
+
+    public TreeNode getNewNode() {
+        return newNode;
+    }
+
+    public void setNewNode(TreeNode newNode) {
+        this.newNode = newNode;
+    }
+
+    public TreeNode getRoot() {
+        return root;
+    }
+
+    public void setRoot(TreeNode root) {
+        this.root = root;
+    }
+
+    public TreeNode getTemp() {
+        return temp;
+    }
+
+    public void setTemp(TreeNode temp) {
+        this.temp = temp;
+    }
+
+    public MyQueue getPrefix() {
+        return prefix;
+    }
+
+    public void setPrefix(MyQueue prefix) {
+        this.prefix = prefix;
+    }
+
+    public MyQueue getPostfix() {
+        return postfix;
+    }
+
+    public void setPostfix(MyQueue postfix) {
         this.postfix = postfix;
     }
 }
