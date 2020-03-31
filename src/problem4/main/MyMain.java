32baf2716506359880766a55e18234ac6a14c6e7
Vaibhav <vaibhav.singh_cs18@gla.ac.in>
2020-03-31 18:41:29 +0530

    declaring scanner instance of Scanner class and binarySearch of MyBinarySearchTree and Myqueue instance queue

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -5,7 +5,18 @@
  *  Time: 7:08 PM
  */
 package problem4.main;
+
+import problem1.mybst.MyBinarySearchTree;
+import problem4.myqueue.MyQueue;
+
+import java.util.Scanner;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        MyBinarySearchTree binarySearch;
+        MyQueue queue;
+    }
 }
