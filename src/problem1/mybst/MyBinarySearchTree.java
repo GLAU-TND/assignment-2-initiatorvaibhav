ea904045a8956f4905816428dc249f67efd539ac
Vaibhav <vaibhav.singh_cs18@gla.ac.in>
2020-03-31 18:51:55 +0530

    defining constructor MyBinarySearchTree.java

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -7,10 +7,22 @@
 package problem1.mybst;
 
 import problem1.node.TreeNode;
+import problem4.myqueue.MyQueue;
 
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
     private TreeNode newNode;
     private TreeNode root;
     private TreeNode temp;
+    private int count;
+    private MyQueue prefix;
+    private MyQueue postfix;
+
+    public MyBinarySearchTree() {
+        this.root = root;
+        this.temp = temp;
+        this.count = 0;
+        this.prefix = prefix;
+        this.postfix = postfix;
+    }
 }
