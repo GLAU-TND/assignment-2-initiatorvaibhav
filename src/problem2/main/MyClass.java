f5bc5935ae9e6dd1fc7de2e05f737b23208aad90
Vaibhav <vaibhav.singh_cs18@gla.ac.in>
2020-03-31 20:55:44 +0530

    defining public class MyClass and private instances MyBinarySearchTree,Preix,Postfix.

---
+++ src/problem2/main/MyClass.java
@@ -0,0 +1,12 @@
+package problem2.main;
+
+import problem1.mybst.MyBinarySearchTree;
+import problem4.myqueue.MyQueue;
+
+public class MyClass {
+    private MyBinarySearchTree binarySearchTree;
+    private MyQueue prefix;
+    private MyQueue postFix;
+
+
+}
